50b08e266fd83bbe2797e5c2e6a672a41bb6427f
valentina <valentinamarcante08@gmail.com>
2025-07-31 11:30:23 -0300

    aula de hoje

---
+++ comando_input.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+    
+    int a = 5;
+    int b = 10;
+    
+    printf("DIgite o valor de A: ");
+    scanf("%d", &a);
+    
+    printf("Digite o valor de B:");
+    scanf("%d", &b);
+    
+    int soma = a + b;
+    
+    
+    printf("\n");
+    
+    printf("%d x %d = %d",a , b, soma);
+}
