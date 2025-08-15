b5f29a79070367da502715a195f47d991966b31b
valentina <valentinamarcante08@gmail.com>
2025-08-15 08:42:36 -0300

    15/08

---
+++ do_while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+int i = 0;
+    printf("\nIncrementa de 0 a 10");
+do{
+    printf("%d\n", i);
+    i++;
+}while(i <=10);
+
+printf("\nDecrementa de 11 a 0");
+do{
+    printf("%d\n",i);
+    i--;
+    }while(i>=0);
+    
+    printf("Qual é o valor de i 7 : %d", i);
+}
---
+++ while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+ printf("Estrutura While");
+int i = 0;
+    
+while(i <= 10){
+ printf("\n%d", i);
+ i++;
+    }
+}
