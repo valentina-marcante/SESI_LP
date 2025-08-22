f131f4125d4db7ff52a203898d8c052d03e77d1c
valentina <valentinamarcante08@gmail.com>
2025-08-22 11:14:14 -0300

    Aula 22/08

--- for.c
+++ for.c
@@ -1,22 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
 void main(){
-    char nome[100] = "Valentina";
+    int contador = 0;
     
-    /*printf("%c\n", nome[0]);
-    printf("%c\n", nome[1]);
-    printf("%c\n", nome[2]);
-    printf("%c\n", nome[3]);
-    printf("%c\n", nome[4]);
-    printf("%c\n", nome[5]);
-    printf("%c\n", nome[6]);
-    printf("%c\n", nome[7]);
-    printf("%c\n", nome[8]);*/
-
-for(int i = 0; i<= 8; i = i +1){
-    //printf("%d\n", i);
-    printf("%c\n", nome[i]);
-}
-
-
+    printf("Digite o numero: ");
+    scanf("%d", &contador);
+    
+    for(int i =1; i <= contador; i++){
+    printf("%d\n", i);
+    }
 }
